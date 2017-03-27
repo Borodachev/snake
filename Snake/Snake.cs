448c856a25dc55eebb2929365398b07d316a797f
Максим Бородачев <MBorodachev@KSP.local>
2017-03-27 10:23:51 +0400

    Создание пробного файла

---
+++ Snake/Snake.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+            Console.ReadLine();
+        }
+    }
+}
